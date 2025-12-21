cdfd2ecbf943af88cad32a7071eea3dfd217f981
Andreas Bröder <andibroeder@gmail.com>
2025-12-21 12:03:12 +0100

    Initial Commit

---
+++ CleanYourBoard - Keyboard Cleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanYourBoard - Keyboard Cleaner
+//
+//  Created by Andy Broeder on 21.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
